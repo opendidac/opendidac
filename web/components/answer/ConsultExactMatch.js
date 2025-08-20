e62ee464b2d91100474cf40b8138b4e64c7d96b3
OlivierLmr <olivier.lemer@alumni.epfl.ch>
2025-08-20 11:57:52 +0200

    Consult component

---
+++ web/components/answer/ConsultExactMatch.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2022-2024 HEIG-VD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Stack, TextField } from '@mui/material'
+import MarkdownViewer from '@/components/input/markdown/MarkdownViewer'
+import ScrollContainer from '@/components/layout/ScrollContainer'
+import { styled } from '@mui/system'
+
+const MultiLineTextField = styled(TextField)({
+  '& textarea': {
+    whiteSpace: 'pre-wrap', // Preserves whitespaces and wraps text
+  },
+})
+
+const ConsultExactMatch = ({ exactMatch, answer }) => {
+  return (
+    <Stack p={2} pt={2} height={'100%'} spacing={2}>
+      <ScrollContainer>
+        <Stack spacing={2} width={'100%'}>
+          {exactMatch.fields.map((field) => {
+            let { value: fieldAnswer } = answer.fields.find(
+              (ans) => ans.fieldId === field.id,
+            ) || { value: '' }
+            return (
+              <Stack key={field.id} spacing={1} width={'100%'} pb={2}>
+                <MarkdownViewer content={field.statement} />
+                  <MultiLineTextField
+                    label={'Answer'}
+                    multiline
+                    variant="standard"
+                    fullWidth
+                    value={fieldAnswer || ''}
+                  />
+              </Stack>
+            )
+          })}
+        </Stack>
+      </ScrollContainer>
+    </Stack>
+  )
+}
+
+export default ConsultExactMatch
