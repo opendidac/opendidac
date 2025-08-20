2cc6ad1a665cfe5d0c7dbafb053e5d07dd5b9835
OlivierLmr <olivier.lemer@alumni.epfl.ch>
2025-08-20 11:43:18 +0200

    Compare component

---
+++ web/components/answer/CompareExactMatch.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2022-2024 HEIG-VD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Alert, Stack, TextField } from '@mui/material'
+import ResizePanel from '@/components/layout/utils/ResizePanel'
+import ScrollContainer from '../layout/ScrollContainer'
+import MarkdownViewer from '@/components/input/markdown/MarkdownViewer'
+import { styled } from '@mui/system'
+
+const MultiLineTextField = styled(TextField)({
+  '& textarea': {
+    whiteSpace: 'pre-wrap', // Preserves whitespaces and wraps text
+  },
+})
+// Styled component to apply whitespace visibility
+const MonoSpaceTextField = styled(TextField)({
+  '& textarea': {
+    whiteSpace: 'pre-wrap', // Preserves whitespaces and wraps text
+    fontFamily: 'monospace', // Makes spaces more noticeable
+  },
+})
+
+const CompareExactMatch = ({
+  solution,
+  answer,
+}) => {
+  return (
+    <Stack p={2} pt={2} height={'100%'} spacing={2}>
+      <ScrollContainer>
+        <Stack spacing={2} width={'100%'}>
+          {solution.fields.map((field, index) => {
+            let { value: fieldAnswer } = answer.fields.find(
+              (ans) => ans.fieldId === field.id,
+            ) || { value: '' }
+            const parts = field.matchRegex.match(/^\/(.*)\/([a-z]*)$/)
+            let regex = (!parts || parts.length !== 3) ?
+              new RegExp(field.matchRegex) :
+              new RegExp(parts[1], parts[2])
+            let matches = fieldAnswer.match(regex)
+            return (
+              <Stack key={field.id} spacing={1} width={'100%'} pb={2}>
+                <MarkdownViewer content={field.statement} />
+                <Alert
+                  severity={matches ? "success" : "error"}
+                  variant="standard"
+                  icon={false}
+                  sx={{
+                    px: 1,
+                    '& .MuiAlert-message': {
+                      width: '100%',
+                    },
+                  }}
+                >
+                <ResizePanel
+                  hideHandle={true}
+                  leftPanel={
+                    <MultiLineTextField
+                      label={'Answer'}
+                      multiline
+                      variant="standard"
+                      fullWidth
+                      value={fieldAnswer || ''}
+                    />
+                  }
+                  rightPanel={
+                    <MonoSpaceTextField
+                      id={`regex-${field.id}`}
+                      variant="standard"
+                      label={'Expected Answer (Regex)'}
+                      value={field.matchRegex}
+                      fullWidth
+                      error={false}
+                    />
+                  }
+                />
+                </Alert>
+              </Stack>
+            )
+          })}
+        </Stack>
+      </ScrollContainer>
+    </Stack>
+  )
+}
+
+export default CompareExactMatch
