fdb81de528f3a49ee3aa90c23ba5c11857e60f5b
OlivierLmr <olivier.lemer@alumni.epfl.ch>
2025-08-21 15:36:42 +0200

    Single migration file

--- web/prisma/migrations/20250819130037_add_relation_to_field_in_answer/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "StudentAnswerExactMatchField" ADD CONSTRAINT "StudentAnswerExactMatchField_fieldId_fkey" FOREIGN KEY ("fieldId") REFERENCES "ExactMatchField"("id") ON DELETE CASCADE ON UPDATE CASCADE;
