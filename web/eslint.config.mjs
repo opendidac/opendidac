aa2e561035122d31ad6e0c880e03b5d9931d9998
Stefan Teofanovic <stefanteofanovic@hotmail.com>
2025-10-21 11:41:56 +0200

    revert linte

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import header from "eslint-plugin-header";
 import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
 export default defineConfig([{
-    extends: [...nextCoreWebVitals, ...compat.extends("prettier")],
+    extends: compat.extends("next/core-web-vitals"),
 
     plugins: {
         header,
